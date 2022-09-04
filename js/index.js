08f3cf10859b07f67be06e714a9e3c9edbb426fd
axtuki1 <sakura.000.aki@gmail.com>
2022-09-04 15:18:44 +0900

    色、ファイル名指定の追加 CSSをちゃんと入れました

--- js/index.js
+++ js/index.js
@@ -6,11 +6,13 @@ const imageGen = (width, height) => {
     canvas.width = width;
     canvas.height = height;
     const context = canvas.getContext("2d");
-    context.fillStyle = "#000000";
-    context.fillRect(0,0,width,height);
+    context.fillStyle = document.querySelector("#color").value;
+    context.fillRect(0, 0, width, height);
+    
+    let name = document.querySelector("#name").value.replace(/\.png/g, '');
     let link = document.createElement("a");
     link.href = canvas.toDataURL("image/png");
-    link.download = "empty.png";
+    link.download = name+".png";
     link.click();
 }
 
